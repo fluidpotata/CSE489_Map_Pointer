5335f1cedb11e44feba65447e6d05bff514defe5
Md Ibrahim Alif <md.ibrahim.alif@g.bracu.ac.bd>
2025-08-10 22:06:32 +0600

    Init

---
+++ app/src/main/java/com/fluidpotata/placesapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.fluidpotata.placesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.fluidpotata.placesapp.ui.theme.PlacesAppTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PlacesAppTheme {
+                Greeting("Android")
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
---
+++ app/src/main/java/com/fluidpotata/placesapp/Routes.kt
@@ -0,0 +1,7 @@
+package com.fluidpotata.placesapp
+
+object ScreenRoutes {
+    const val Map = "home"
+    const val Form = "form"
+    const val List = "list"
+}
